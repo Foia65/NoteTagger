c8290d3b5ce73305f35168ca084ae0f9dffd286e
Francesco Foianesi <foiasoft@gmail.com>
2026-06-17 19:31:23 +0200

    Initial Commit

---
+++ NoteTagger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoteTagger
+//
+//  Created by Francesco Foianesi on 17/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoteTagger/NoteTaggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTaggerApp.swift
+//  NoteTagger
+//
+//  Created by Francesco Foianesi on 17/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct NoteTaggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
